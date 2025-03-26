3aa045eaf44e6d472c809886f78669f8d90ba6fe
Shell679 <nyancat2452@gmail.com>
2025-03-26 13:44:43 +0500

    Make repository for wallet query

--- app/schemas/schema.py
+++ app/schemas/schema.py
@@ -1,4 +1,14 @@
+from datetime import datetime
+
 from pydantic import BaseModel
+
+class WalletRead(BaseModel):
+    id: int
+    wallet_address: str
+    balance: float
+    bandwidth: int
+    energy: int
+    created_at: datetime
 
 class WalletCreate(BaseModel):
     wallet_address: str
---
+++ app/v1/repositories/wallet_repository.py
@@ -0,0 +1,26 @@
+from typing import Any, Sequence
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.models import TronWalletQuery
+from app.schemas.schema import WalletCreate, WalletRead
+
+
+class WalletRepository:
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def get_wallets(self) -> Sequence[WalletRead]:
+        data = await self.session.execute(select(TronWalletQuery))
+        wallets = data.scalars().all()
+
+        return wallets
+
+    async def create_wallet_query(self, new_wallet_data: WalletCreate) -> dict[str, Any]:
+        new_wallet = TronWalletQuery(**new_wallet_data.model_dump())
+        self.session.add(new_wallet)
+        await self.session.flush()
+        await self.session.commit()
+
+        return new_wallet
