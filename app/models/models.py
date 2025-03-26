4e2a0d8c96765d4c9cdbef5a72bc19116346515d
Shell679 <nyancat2452@gmail.com>
2025-03-26 13:10:52 +0500

    Make model for database

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,17 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.sql.schema import MetaData
+from sqlalchemy import Column, Integer, String, Float, DateTime, func
 
 models_metadata = MetaData()
 Base = declarative_base(metadata=models_metadata)
+
+class TronWalletQuery(Base):
+    __tablename__ = "tron_wallet_queries"
+    metadata = models_metadata
+
+    id = Column(Integer, primary_key=True, index=True)
+    wallet_address = Column(String, nullable=False)
+    bandwidth = Column(Integer, nullable=False)
+    energy = Column(Integer, nullable=False)
+    balance_trx = Column(Float, nullable=False)
+    created_at = Column(DateTime, default=func.now())
